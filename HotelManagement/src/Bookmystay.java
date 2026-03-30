527dad9e76b68f11ae2312c706f78cf94d9f548d
Sriramsai045 <sriramsai657@gmail.com>
2026-03-30 15:12:32 +0530

    Application Entry & Welcome Message

---
+++ HotelManagement/src/Bookmystay.java
@@ -0,0 +1,6 @@
+public class Bookmystay {
+    public static void main(String[] args){
+        System.out.println("Welcome to Hotel Management System");
+        System.out.println("system Initialised Successfully.");
+    }
+}
